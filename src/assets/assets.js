871859b9a31defe3906825cc8fb12e1436df6305
rinchen29 <rinchen2911@gmail.com>
2025-10-31 09:43:50 -0700

    fixed image path

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,5 +1,5 @@
 import gradient from "../assets/gradient.png";
-import profile from "../assets/profile.png"
+import profile from "../assets/Profile.png"
 export const assets={
     gradient,
     profile
